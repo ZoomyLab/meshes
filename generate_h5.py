2d0e53b7d2575c59608c11f582bcb55e1dc54b29
ingo.steldermann <steldermann@mbd.rwth-aachen.de>
2026-04-10 05:52:04 +0200

    Use BaseMesh.from_msh() for H5 generation (no PETSc)

--- generate_h5.py
+++ generate_h5.py
@@ -1,5 +1,17 @@
+"""Convert .msh files to HDF5 using the new mesh hierarchy (no PETSc)."""
+
 import glob
-from zoomy_core.mesh.mesh import msh_to_h5
+import os
+from zoomy_core.mesh import BaseMesh
+
+
+def msh_to_h5(msh_path):
+    """Convert a .msh file to .h5 via BaseMesh (topology-only, format v2)."""
+    h5_path = os.path.splitext(msh_path)[0] + ".h5"
+    mesh = BaseMesh.from_msh(msh_path)
+    mesh.write_to_hdf5(h5_path)
+    return h5_path
+
 
 files = glob.glob("**/*.msh", recursive=True)
 for f in files:
